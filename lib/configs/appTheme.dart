5c654358bb8a91ee195a10b1d1c49af5d642ea6a
黒狗さん。 <kuroinusanshinji@gmail.com>
2024-11-11 16:19:25 +0900

    todoの根本部分を作成

--- lib/configs/appTheme.dart
+++ lib/configs/appTheme.dart
@@ -1,18 +1,20 @@
 import 'package:flutter/material.dart';
 
+const Color primaryColor = Color(0xFFF3F3F3);
+
 final appTheme = ThemeData(
   fontFamily: 'MPLUSRounded1c-Regular',
   colorScheme: const ColorScheme(
     brightness: Brightness.light,
     primary: Color(0xFF000000),
-    onPrimary: Color(0xFF000000),
+    onPrimary: Color(0xFFF3F3F3),
     secondary: Color(0xFF000000),
-    onSecondary: Color(0xFFFFFFFF),
+    onSecondary: Color(0xFFF3F3F3),
     error: Color(0xFFF2CB57),
     onError: Color(0xFFFFFFFF),
-    background: Color(0xFFFFFDFB),
-    onBackground: Color(0xFFFFFDFB),
-    surface: Color(0xFFFFFDFB),
+    background: Color(0xFFF3F3F3),
+    onBackground: Color(0xFFF3F3F3),
+    surface: Color(0xFFF3F3F3),
     onSurface: Color(0xFF000000),
   ),
 );
--- lib/screens/todoMain.dart
+++ lib/screens/todoMain.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:testudy/configs/appTheme.dart';
 
 import '../class/todoItem.dart';
 
@@ -11,7 +12,7 @@ class TodoMain extends StatefulWidget {
 
 class _TodoMainState extends State<TodoMain> {
   //finalは一時的にUIを更新するためのもの
-  final List<TodoItem> _todoItems = [
+  List<TodoItem> _todoItems = [
     TodoItem(
       title: '数学ワーク',
       description: '',
@@ -21,33 +22,98 @@ class _TodoMainState extends State<TodoMain> {
     ),
     TodoItem(
       title: '英語ワーク',
-      description: '',
+      description: 'zoomで確認',
       subject: 'Eng',
       endDate: DateTime.now(),
       isDone: false,
     ),
     TodoItem(
       title: '英単語',
-      description: '',
+      description: '1000~1500',
       subject: 'Eng',
       endDate: DateTime.now(),
       isDone: false,
     ),
   ];
+
+  List<TodoItem> DoneItems = [];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Row(
+        title: const Column(
           children: [
-            Icon(Icons.check_box,size: 30,),
-            SizedBox(width: 30),
-            Text('Todo',style: TextStyle(fontSize:25)),
+            Row(
+              children: [
+                Icon(
+                  Icons.check_box,
+                  size: 30,
+                ),
+                SizedBox(width: 30),
+                Text('Todo', style: TextStyle(fontSize: 25)),
+              ],
+            ),
+            SizedBox(height: 5),
           ],
+        ),
+        shape: const Border(
+          bottom: BorderSide(
+            color: Color(0xFFCCCCCC),
+            width: 1.5,
+          ),
         ),
       ),
       body: Column(
-
+        children: [
+          SizedBox(height: 10),
+          Expanded(
+            child: ListView.builder(
+              itemCount: _todoItems.length,
+              itemBuilder: (context, index) {
+                return Card(
+                  elevation: 5,
+                  shadowColor: Colors.transparent,
+                  color: primaryColor,
+                  child: ListTile(
+                    title: Text(
+                      _todoItems[index].title,
+                      style: TextStyle(
+                        decoration: _todoItems[index].isDone
+                            ? TextDecoration.lineThrough
+                            : null,
+                        fontSize: 20,
+                      ),
+                    ),
+                    subtitle: _todoItems[index].description != ""
+                        ? Text(
+                      _todoItems[index].description,
+                      style: TextStyle(
+                        decoration: _todoItems[index].isDone
+                            ? TextDecoration.lineThrough
+                            : null,
+                      ),
+                    )
+                        : null,
+                    leading: Transform.scale(
+                      scale: 1.3,
+                      child: Checkbox(
+                        value: _todoItems[index].isDone,
+                        //fillColor: MaterialStateProperty.all(primaryColor),
+                        checkColor: primaryColor,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            _todoItems[index].isDone = value!;
+                          });
+                        },
+                      ),
+                    ),
+                  ),
+                );
+              },
+            ),
+          ),
+        ],
       ),
     );
   }
