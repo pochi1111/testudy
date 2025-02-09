9b3bbb6d53a7868dc1b3d6a9fd04d9459cd928ac
黒狗さん。 <kuroinusanshinji@gmail.com>
2025-02-09 11:27:39 +0900

    [fix] グラフの修正

--- lib/screens/elements/weekTimeGraph.dart
+++ lib/screens/elements/weekTimeGraph.dart
@@ -10,7 +10,7 @@ class WeekTimeGraph extends StatelessWidget {
     required this.studyTimesInt,
   }) : super(key: key);
 
-  static const double barWidth = 20.0;
+  static const double barWidth = 25.0;
   final TextStyle _labelStyle =
       const TextStyle(fontSize: 16, fontWeight: FontWeight.w800);
 
@@ -19,27 +19,31 @@ class WeekTimeGraph extends StatelessWidget {
     final maxY = studyTimesInt
         .reduce((value, element) => value > element ? value : element)
         .toDouble();
-    late int interval;
+    late final int interval;
     if (maxY < 60) {
       interval = 10;
     } else if (maxY < 120) {
       interval = 20;
-    } else {
+    } else if (maxY < 180) {
       interval = 30;
+    } else if (maxY < 240) {
+      interval = 40;
+    } else {
+      interval = 60;
     }
     return BarChart(
       BarChartData(
         gridData: FlGridData(
+          horizontalInterval: interval.toDouble(),
           show: true,
-          checkToShowHorizontalLine: (value) => value.toInt() % interval == 0,
           getDrawingHorizontalLine: (value) => FlLine(
             color: appTheme.primaryColor.withValues(alpha: 0.1),
-            strokeWidth: 1,
+            strokeWidth: value.toInt() % 60 == 0 ? 1.4 : 1,
           ),
           drawVerticalLine: false,
         ),
         borderData: FlBorderData(
-          show: true,
+          show: false,
           border: Border(
             bottom: BorderSide(
               color: appTheme.primaryColor.withValues(alpha: 0.3),
@@ -54,77 +58,26 @@ class WeekTimeGraph extends StatelessWidget {
           touchTooltipData: BarTouchTooltipData(
             tooltipMargin: 0,
             getTooltipColor: (_) => Colors.transparent,
-            getTooltipItem: (group, groupIndex, rod, rodIndex) {
-              final minutes = studyTimesInt[group.x.toInt()];
-              final hours = minutes ~/ 60;
-              final remainingMinutes = minutes % 60;
-              late String txt;
-              if (hours > 0) {
-                if (remainingMinutes > 0) {
-                  txt = '$hours時間\n$remainingMinutes分';
-                } else {
-                  txt = '$hours時間';
-                }
-              } else {
-                txt = '$remainingMinutes分';
-              }
-              return BarTooltipItem(
-                txt,
-                const TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.w500,
-                ),
-              );
-            },
+            getTooltipItem: toolTipItem,
           ),
         ),
         titlesData: FlTitlesData(
           show: true,
-          rightTitles: const AxisTitles(
-            sideTitles: SideTitles(showTitles: false),
-          ),
-          topTitles: const AxisTitles(
-            sideTitles: SideTitles(showTitles: false),
-          ),
+          rightTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
+          topTitles: const AxisTitles(sideTitles: SideTitles(showTitles: false)),
           leftTitles: AxisTitles(
             sideTitles: SideTitles(
-              showTitles: true,
               maxIncluded: false,
-              minIncluded: false,
-              reservedSize: 50,
+              showTitles: true,
+              reservedSize: 55,
               interval: interval.toDouble(),
-              getTitlesWidget: (value, meta) {
-                final minutes = value.toInt();
-                final hours = minutes ~/ 60;
-                final remainingMinutes = minutes % 60;
-                String txt;
-                if (hours > 0) {
-                  if (remainingMinutes > 0) {
-                    txt = '$hours時間\n$remainingMinutes分';
-                  } else {
-                    txt = '$hours時間';
-                  }
-                } else {
-                  txt = '$minutes分';
-                }
-                return SideTitleWidget(
-                  meta: meta,
-                  child: Text(
-                    txt,
-                    style: TextStyle(
-                      color: appTheme.primaryColor,
-                      fontSize: 12,
-                      fontWeight: FontWeight.w600,
-                    ),
-                  ),
-                );
-              },
+              getTitlesWidget: leftTitle,
             ),
           ),
           bottomTitles: AxisTitles(
             sideTitles: SideTitles(
               showTitles: true,
-              getTitlesWidget: getTitles,
+              getTitlesWidget: bottomTitle,
               reservedSize: 25,
             ),
           ),
@@ -136,10 +89,58 @@ class WeekTimeGraph extends StatelessWidget {
     );
   }
 
-  Widget getTitles(double value, TitleMeta meta) {
+  Widget leftTitle(value, meta) {
+              final minutes = value.toInt();
+              final hours = minutes ~/ 60;
+              final remainingMinutes = minutes % 60;
+              String txt;
+              if (hours > 0) {
+                if (remainingMinutes > 0) {
+                  txt = '$hours時間\n$remainingMinutes分';
+                } else {
+                  txt = '$hours時間';
+                }
+              } else {
+                txt = '$minutes分';
+              }
+              return SideTitleWidget(
+                meta: meta,
+                child: Text(
+                  txt,
+                  style: TextStyle(
+                    color: appTheme.primaryColor,
+                    fontSize: 12,
+                  ),
+                ),
+              );
+            }
+
+  BarTooltipItem? toolTipItem(group, groupIndex, rod, rodIndex) {
+    final minutes = studyTimesInt[group.x.toInt()];
+    final hours = minutes ~/ 60;
+    final remainingMinutes = minutes % 60;
+    late String txt;
+    if (hours > 0) {
+      if (remainingMinutes > 0) {
+        txt = '$hours時間\n$remainingMinutes分';
+      } else {
+        txt = '$hours時間';
+      }
+    } else {
+      txt = '$remainingMinutes分';
+    }
+    return BarTooltipItem(
+      txt,
+      const TextStyle(
+        color: Colors.black,
+        fontWeight: FontWeight.w500,
+      ),
+    );
+  }
+
+  Widget bottomTitle(double value, TitleMeta meta) {
     final style = TextStyle(
       color: appTheme.primaryColor,
-      fontWeight: FontWeight.w600,
       fontSize: 14,
     );
     Widget text;
@@ -158,7 +159,7 @@ class WeekTimeGraph extends StatelessWidget {
           BarChartRodData(
               toY: studyTimesInt[i].toDouble(),
               width: barWidth,
-              borderRadius: const BorderRadius.all(Radius.zero))
+              borderRadius: const BorderRadius.all(Radius.zero)),
         ]);
       });
 }
