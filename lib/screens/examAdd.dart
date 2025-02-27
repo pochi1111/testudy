c0fd5c595698573cc2c2507ef13517ecda338f2f
黒狗さん。 <kuroinusanshinji@gmail.com>
2025-02-27 23:19:53 +0900

    ✅ [add] テストの追加を追加

---
+++ lib/screens/examAdd.dart
@@ -0,0 +1,244 @@
+import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:testudy/Configs/appTheme.dart';
+import 'package:testudy/services/examSchedule.dart';
+
+class ExamAdd extends StatefulWidget {
+  final DateTime initialDate;  // 初期日付を追加
+
+  const ExamAdd({
+    Key? key,
+    required this.initialDate,
+  }) : super(key: key);
+
+  @override
+  State<ExamAdd> createState() => _ExamAddState();
+}
+
+class _ExamAddState extends State<ExamAdd> {
+  late DateTime examDate;
+  final TextEditingController titleController = TextEditingController();
+  final TextEditingController descriptionController = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+    examDate = widget.initialDate;  // 初期値を渡された日付に設定
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        scrolledUnderElevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          margin: const EdgeInsets.all(20.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "テストを追加",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  color: appTheme.colorScheme.primary,
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                margin: const EdgeInsets.only(top: 20.0, left: 10.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      children: [
+                        TextButton(
+                          onPressed: () {
+                            _selectDateTime();
+                          },
+                          style: ButtonStyle(
+                            minimumSize: MaterialStateProperty.all(Size.zero),
+                            padding: MaterialStateProperty.all(EdgeInsets.zero),
+                          ),
+                          child: Container(
+                            width: 220, // 180から220に変更
+                            decoration: BoxDecoration(
+                              border: Border(
+                                bottom: BorderSide(
+                                  color: appTheme.colorScheme.primary,
+                                  width: 2,
+                                ),
+                              ),
+                            ),
+                            padding: const EdgeInsets.only(bottom: 3),
+                            child: Row(
+                              children: [
+                                const SizedBox(width: 10),
+                                const Icon(Icons.calendar_today, size: 25),
+                                const SizedBox(width: 15),
+                                Text(
+                                  DateFormat("yyyy年MM月dd日").format(examDate),
+                                  style: const TextStyle(fontSize: 20),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                        IconButton(
+                          icon: const Icon(Icons.refresh),
+                          iconSize: 30,
+                          onPressed: () {
+                            setState(() {
+                              examDate = DateTime.now();
+                            });
+                          },
+                        ),
+                      ],
+                    ),
+                    const SizedBox(height: 20),
+                    SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: titleController,
+                        decoration: InputDecoration(
+                          prefixIcon: const Icon(Icons.title, size: 25),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: appTheme.colorScheme.primary,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(
+                              color: appTheme.colorScheme.primary,
+                              width: 2,
+                            ),
+                          ),
+                          hintText: "テスト名",
+                        ),
+                        style: const TextStyle(fontSize: 20),
+                      ),
+                    ),
+                    const SizedBox(height: 20),
+                    const Text("メモ", style: TextStyle(fontSize: 20)),
+                    const SizedBox(height: 15),
+                    SizedBox(
+                      width: 300,
+                      child: TextField(
+                        controller: descriptionController,
+                        maxLines: 5,
+                        decoration: InputDecoration(
+                          border: const OutlineInputBorder(),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: appTheme.colorScheme.primary,
+                              width: 2,
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: appTheme.colorScheme.primary,
+                              width: 2,
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 40),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: ElevatedButton(
+                  onPressed: _addExam,
+                  style: ElevatedButton.styleFrom(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(10),
+                    ),
+                    backgroundColor: appTheme.colorScheme.primary,
+                  ),
+                  child: Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    child: Row(
+                      mainAxisSize: MainAxisSize.min,
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Icon(
+                          Icons.add,
+                          size: 28,
+                          color: appTheme.colorScheme.onPrimary,
+                        ),
+                        Text(
+                          " 追加 ",
+                          style: TextStyle(
+                            fontSize: 28,
+                            color: appTheme.colorScheme.onPrimary,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _selectDateTime() async {
+    final DateTime? date = await showDatePicker(
+      context: context,
+      initialDate: examDate,
+      firstDate: DateTime.now(),
+      lastDate: DateTime.now().add(const Duration(days: 365)),
+    );
+
+    if (date != null) {
+      setState(() {
+        examDate = DateTime(date.year, date.month, date.day);
+      });
+    }
+  }
+
+  void _addExam() {
+    if (titleController.text.isEmpty) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text("テスト名を入力してください"),
+          duration: Duration(milliseconds: 1500),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
+          ),
+        ),
+      );
+      return;
+    }
+
+    ExamAPI().addExam(
+      Exam(
+        title: titleController.text,
+        description: descriptionController.text,
+        dateTime: examDate,
+      ),
+    );
+
+    ScaffoldMessenger.of(context).showSnackBar(
+      const SnackBar(
+        content: Text("テストを追加しました"),
+        duration: Duration(milliseconds: 1500),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.vertical(top: Radius.circular(5)),
+        ),
+      ),
+    );
+    
+    Navigator.of(context).pop(true);
+  }
+}
--- lib/screens/examScreen.dart
+++ lib/screens/examScreen.dart
@@ -3,6 +3,7 @@ import 'dart:collection';
 import 'package:flutter/material.dart';
 import 'package:table_calendar/table_calendar.dart';
 import 'package:testudy/configs/appTheme.dart';
+import 'package:testudy/screens/examAdd.dart';
 import 'package:testudy/services/examSchedule.dart';
 
 class ExamScreen extends StatefulWidget {
@@ -15,6 +16,7 @@ class ExamScreen extends StatefulWidget {
 class _ExamScreenState extends State<ExamScreen> {
   DateTime _selectedDay = DateTime.now();
   DateTime _focusedDay = DateTime.now();
+  List<Exam> _selectedExams = [];
   List<Exam> _exams = [];
   LinkedHashMap<DateTime, List<Exam>> _examsMap = LinkedHashMap();
 
@@ -31,67 +33,132 @@ class _ExamScreenState extends State<ExamScreen> {
         appBar: AppBar(
           toolbarHeight: 0,
         ),
-        body: Column(
-          children: [
-            //ここに次の最短のテストまで何日か、又その詳細を表示する
-            TableCalendar(
-              onPageChanged: (focusedDay) {
-                _updateExams(focusedDay);
-              },
-              eventLoader: (day) {
-                day = DateTime(day.year, day.month, day.day);
-                return _examsMap[day] ?? [];
-              },
-              headerStyle: HeaderStyle(
-                formatButtonVisible: false,
-                titleCentered: true,
-                titleTextStyle: TextStyle(
-                  color: appTheme.colorScheme.primary,
-                  fontSize: 20,
+        body: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: MediaQuery.of(context).size.height * 0.4,
+                child: TableCalendar(
+                  calendarBuilders: CalendarBuilders(
+                    markerBuilder: (context, day, events) {
+                      if (events.isEmpty) {
+                        return Container();
+                      }
+                      return Positioned(
+                        top: 8,
+                        right: 10,
+                        child: Container(
+                          margin: const EdgeInsets.all(2),
+                          decoration: const BoxDecoration(
+                            color: Color.fromARGB(255, 231, 150, 150),
+                            shape: BoxShape.circle,
+                          ),
+                          width: 10,
+                          height: 10,
+                        ),
+                      );
+                    },
+                  ),
+                  onPageChanged: (focusedDay) {
+                    _updateExams(focusedDay);
+                  },
+                  eventLoader: (day) {
+                    day = DateTime(day.year, day.month, day.day);
+                    return _examsMap[day] ?? [];
+                  },
+                  headerStyle: HeaderStyle(
+                    formatButtonVisible: false,
+                    titleCentered: true,
+                    titleTextStyle: TextStyle(
+                      color: appTheme.colorScheme.primary,
+                      fontSize: 20,
+                    ),
+                  ),
+                  daysOfWeekStyle: DaysOfWeekStyle(
+                    weekdayStyle: TextStyle(
+                      color: appTheme.colorScheme.primary,
+                    ),
+                    weekendStyle: TextStyle(
+                      color: appTheme.colorScheme.primary,
+                    ),
+                  ),
+                  daysOfWeekHeight: MediaQuery.of(context).size.height * 0.03,
+                  calendarStyle: CalendarStyle(
+                    todayDecoration: BoxDecoration(
+                      color: appTheme.colorScheme.primary.withOpacity(0.2),
+                      shape: BoxShape.circle,
+                    ),
+                    selectedDecoration: BoxDecoration(
+                      color: appTheme.colorScheme.primary,
+                      shape: BoxShape.circle,
+                    ),
+                    selectedTextStyle: TextStyle(color: Colors.white),
+                    outsideDaysVisible: false,
+                    weekendTextStyle:
+                        TextStyle(color: appTheme.colorScheme.primary),
+                  ),
+                  rowHeight: MediaQuery.of(context).size.height * 0.05,
+                  firstDay: DateTime.utc(2020, 1, 1),
+                  lastDay: DateTime.utc(2040, 1, 1),
+                  focusedDay: _focusedDay,
+                  selectedDayPredicate: (day) {
+                    return isSameDay(_selectedDay, day);
+                  },
+                  onDaySelected: (selectedDay, focusedDay) {
+                    setState(() {
+                      _selectedDay = selectedDay;
+                      _focusedDay = focusedDay;
+                    });
+                    _updateSelectedExams();
+                  },
+                  calendarFormat: CalendarFormat.month,
+                  startingDayOfWeek: StartingDayOfWeek.sunday,
+                  locale: 'ja_JP',
                 ),
               ),
-              daysOfWeekStyle: DaysOfWeekStyle(
-                weekdayStyle: TextStyle(
-                  color: appTheme.colorScheme.primary,
-                ),
-                weekendStyle: TextStyle(
-                  color: appTheme.colorScheme.primary,
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 8.0),
+                child: Text(
+                  '${_selectedDay.month}月${_selectedDay.day}日',
+                  style: const TextStyle(
+                    fontSize: 17,
+                    height: 2,
+                  ),
                 ),
               ),
-              daysOfWeekHeight: MediaQuery.of(context).size.height * 0.02,
-              calendarStyle: CalendarStyle(
-                todayDecoration: BoxDecoration(
-                  color: appTheme.colorScheme.primary.withOpacity(0.2),
-                  shape: BoxShape.circle,
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: ListView.builder(
+                  itemCount: _selectedExams.length,
+                  //ここのshrinkwrapは検討の余地あり
+                  shrinkWrap: true,
+                  itemBuilder: (context, index) => Card(
+                    child: ListTile(
+                      title: Text(_selectedExams[index].title),
+                      subtitle: Text(_selectedExams[index].description),
+                    ),
+                  ),
                 ),
-                selectedDecoration: BoxDecoration(
-                  color: appTheme.colorScheme.primary,
-                  shape: BoxShape.circle,
-                ),
-                selectedTextStyle: TextStyle(color: Colors.white),
-                outsideDaysVisible: false,
-                weekendTextStyle:
-                    TextStyle(color: appTheme.colorScheme.primary),
               ),
-              rowHeight: MediaQuery.of(context).size.height * 0.05,
-              firstDay: DateTime.utc(2020, 1, 1),
-              lastDay: DateTime.utc(2040, 1, 1),
-              focusedDay: _focusedDay,
-              selectedDayPredicate: (day) {
-                return isSameDay(_selectedDay, day);
-              },
-              onDaySelected: (selectedDay, focusedDay) {
-                setState(() {
-                  _selectedDay = selectedDay;
-                  _focusedDay = focusedDay;
-                });
-              },
-              calendarFormat: CalendarFormat.month,
-              startingDayOfWeek: StartingDayOfWeek.sunday,
-              locale: 'ja_JP',
-            )
-            //ここに選択された日付のテストの詳細を表示する
-          ],
+            ],
+          ),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () async {
+            final result = await Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => ExamAdd(initialDate: _selectedDay),  // 選択された日付を渡す
+              ),
+            );
+            if (result == true) {
+              _updateExams(_focusedDay);
+              _updateSelectedExams();
+              setState(() {});
+            }
+          },
+          child: Icon(Icons.add),
         ));
   }
 
@@ -108,5 +175,12 @@ class _ExamScreenState extends State<ExamScreen> {
       }
       _examsMap[date]!.add(exam);
     }
+  }
+
+  void _updateSelectedExams() {
+    _selectedExams = _examsMap[DateTime(
+            _selectedDay.year, _selectedDay.month, _selectedDay.day)] ??
+        [];
+    setState(() {});
   }
 }
